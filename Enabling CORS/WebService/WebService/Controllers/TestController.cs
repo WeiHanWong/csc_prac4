2dc6504961a6f6e275c1e32da2d73504a455ca9c
WeiHanWong <56218253+WeiHanWong@users.noreply.github.com>
2019-11-15 23:04:21 +0800

    cors

---
+++ Enabling CORS/WebService/WebService/Controllers/TestController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebService.Controllers
+{
+    [EnableCors(origins: "http://localhost:51497", headers: "*", methods: "*", exposedHeaders: "X-Custom-Header", SupportsCredentials = true)]
+    public class TestController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            var resp = new HttpResponseMessage()
+            {
+                Content = new StringContent("GET: Test message")
+            };
+            resp.Headers.Add("X-Custom-Header", "hello");
+            return resp;
+        }
+
+        public HttpResponseMessage Post()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("POST: Test message")
+            };
+        }
+
+        public HttpResponseMessage Put()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("PUT: Test message")
+            };
+        }
+    }
+}
